2ac3663b06add0e2447367d316fa3e0770c51e0a
Admir <adopilot@gmail.com>
2018-12-21 08:20:12 +0100

    Update plugin.rb
    registrovao ikonme

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,5 @@
 # version: 0.1
 # authors: Admir Hodžič
 # url: http://forum.robot.ba/t/aplikacija-mjernjea-utroska-it-resorsa/160902
+register_svg_icon "fa-cart-plus" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-cart-arrow-down" if respond_to?(:register_svg_icon)
